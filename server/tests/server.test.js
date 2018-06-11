b8cf94e0451b77b11be27cae29aa9394ebe9c4c7
Giovani <giovanimdelcol@gmail.com>
2018-06-11 19:43:05 -0300

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,57 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove([])
+  .then(() => {
+      done()
+  });
+  //zera a base antes de cada teste
+});
+
+describe('POST /todos', () => {
+  it('Should create a new To-do', (done) => {
+    var text = 'Todo text - Test';
+
+    request(app)
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+        expect(res.body.text).toBe(text);
+    })
+    .end((err, res)  => {
+        if (err) {
+            return done(err);
+        }
+
+        Todo.find().then((todos) => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+        }).catch((e) => done(e));
+    });
+  });
+
+  it ('Nao deve criar to-do com body invalido', (done) => {
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err, res) => {
+        if (err) {
+            return done(err);
+        }
+
+        Todo.find().then((todos) => {
+            expect(todos.length).toBe(0);
+            done();
+        }).catch((e) => {
+            done(e);
+        })
+    })
+  });
+});
