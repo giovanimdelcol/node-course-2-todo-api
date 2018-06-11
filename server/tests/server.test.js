6533121e812ffe358e4f306e423f463991496510
Giovani <giovanimdelcol@gmail.com>
2018-06-11 20:08:33 -0300

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: "First Dummy"
+}, {
+    text: "Second Dummy"
+}];
+
 beforeEach((done) => {
   Todo.remove([])
   .then(() => {
-      done()
-  });
+    return Todo.insertMany(todos);      
+  }).then(()=> done());
   //zera a base antes de cada teste
 });
 
@@ -28,7 +34,7 @@ describe('POST /todos', () => {
             return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
             done();
@@ -47,7 +53,7 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
         }).catch((e) => {
             done(e);
@@ -55,3 +61,15 @@ describe('POST /todos', () => {
     })
   });
 });
+
+describe('GET /todos', () => {
+    it('Should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+})
