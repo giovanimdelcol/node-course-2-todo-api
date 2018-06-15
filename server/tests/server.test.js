038dd6fd12cf930c545ce205a4011e17b98a3d74
Giovani <giovanimdelcol@gmail.com>
2018-06-15 20:15:34 -0300

    Test the DELETE todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,40 @@ describe('GET /todos/:id',  () => {
         .end(done);        
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('Should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done()
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should return 404 if no to-do found', (done) =>{
+        request(app)
+        .delete(`/todos/${new ObjectId().toHexString()}`)
+        .expect(404)
+        .end(done);   
+    });
+
+    it('should return 404 if id is invalid', (done) =>{
+        request(app)
+        .delete(`/todos/123`)
+        .expect(404)
+        .end(done);   
+    });
+})
