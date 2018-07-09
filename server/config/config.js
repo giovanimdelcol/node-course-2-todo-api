ed99927ec825a388cdc99284b11d319a23d95fe4
Giovani <giovanimdelcol@gmail.com>
2018-07-09 15:56:28 -0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,21 @@
 var env = process.env.NODE_ENV || 'development'; //only exist in heroku
 
-if (env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development'  || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+//abaixo o metodo keys retorna um vetor com as propriedades
+//do objeto. Para cada propriedade nos setaremos
+//uma variavel de ambiente com o mesmo valor de config.
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
